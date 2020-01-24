9a2815ef1993840cd7f7b68bd63070d53f03e65a
Kevin Shah <shahkevin391@gmail.com>
2020-01-24 15:32:17 +0530

    Calender/Calender Practice Task

--- Calender/detailedCalender.php
+++ Calender/detailedCalender.php
@@ -9,9 +9,9 @@
         </head>
         <body>
             <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
-                <input type=text name=year1 placeholder="Starting year" >
+                <input type=text name=year1 placeholder="Starting year" value=<?php if(isset($_POST['year1'])){echo $_POST['year1'];}?> >
                 <br/><br/>
-                <input type=text name=year2 placeholder="Ending year">
+                <input type=text name=year2 placeholder="Ending year" value=<?php if(isset($_POST['year2'])){echo $_POST['year2'];}?>>
                 <br/><br/>
                 <input type=submit name=calender value="Calender">
                 
@@ -24,6 +24,8 @@
  if(isset($_POST['calender'])){
     if(empty($_POST['year1']) || empty($_POST['year2'])){
         echo "*All fields are required*";
+    }else if($_POST['year1'] > $_POST['year2']){
+        echo "Enter Valid Years";
     }
     else{
         $startingYear = $_POST['year1'];
@@ -33,9 +35,11 @@
 }
 function calender($year1, $year2){
     for ($i=$year1; $i <= $year2 ; $i++) { 
+        echo "<h1>$i</h1>";
         for ($j=1; $j <= 12 ; $j++) { 
+            echo "<h2>".date('F', mktime(0,0,0,$j,1,$i))."</h2>";
             $numberOfDays = date('t',mktime(0,0,0,$j,1,$i));
-            echo "<table>";
+            echo "<table border=0 cellpadding=2>";
             echo"<th>Monday</th>";
             echo"<th>Tuesday</th>";
             echo"<th>Wedensday</th>";
@@ -47,26 +51,22 @@ function calender($year1, $year2){
             for ($k=1; $k <= $numberOfDays; $k++) { 
                 echo"<tr>";     
                 for($l=1; $l <= 7; $l++){
-                    $size = date('N',mktime(0,0,0,$j,$i,$k));
-                    // echo $size;
-                    // if($k == $size){
-                //         if($k == $numberOfDays+1){
-                //             break;
-                        // }
-                        echo "<td>".date('D-d-m-y', mktime(0,0,0,$j,$k,$i))."</td>";
-                
-                //         $k++;
-                    // }
-                    // else{
-                        echo "&nbsp";   
-                        //     echo "<td></td>";
-                    // }
+                    $size = date('N',mktime(0,0,0,$j,$k,$i));
+                    if($l == $size){
+                        if($k == $numberOfDays+1){
+                            break;
+                        }
+                        echo "<td align=center>".date('d', mktime(0,0,0,$j,$k,$i))."</td>";
+                        $k++;
+                    }
+                    else{
+                        echo "<td></td>";
+                    }
                 }
-                echo"</tr>";   
-                //  $k = $k-1;
-                // echo"</tr>";       
+                $k = $k-1;
+                echo"</tr>";       
             }
-            echo "</table>";
+            echo "</table><br/>";
         }
     }
 }
